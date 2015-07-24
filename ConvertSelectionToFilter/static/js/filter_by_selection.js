26fbf8caf6d0acb67780df51b4fb56b7e437993b
mehdi samadi <mehsamadi@gmail.com>
2015-07-24 19:01:30 +0430

    Update filter_by_selection.js

--- ConvertSelectionToFilter/static/js/filter_by_selection.js
+++ ConvertSelectionToFilter/static/js/filter_by_selection.js
@@ -1,8 +1,8 @@
 //  @@@ filter by selection JS @@@
 //#############################################################################
 //    
-//    Copyright (C) 2012 Agile Business Group sagl (<http://www.agilebg.com>)
-//    Copyright (C) 2012 Therp BV (<http://therp.nl>)
+//    Copyright (C) 2015 Mehdi Samadi
+//    
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU Affero General Public License as published
