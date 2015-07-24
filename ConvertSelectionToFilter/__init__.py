ad1a0f1a49b0f80b077754a285882ac415d97ddd
mehdi samadi <mehsamadi@gmail.com>
2015-07-24 19:02:59 +0430

    Update and rename SaveSelection/__init__.py to ConvertSelectionToFilter/__init__.py

--- ConvertSelectionToFilter/__init__.py
+++ ConvertSelectionToFilter/__init__.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#
-#    Copyright (C) 2012 Agile Business Group sagl (<http://www.agilebg.com>)
-#    Copyright (C) 2012 Domsense srl (<http://www.domsense.com>)
+#   
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
